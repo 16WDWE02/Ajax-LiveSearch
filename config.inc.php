b465824af667dc7f47eb357d2f7475653e31ff08
Richard Hpa <richard.hpa@hotmail.com>
2016-09-02 08:48:36 +1200

    first commit, set up file structure

---
+++ config.inc.php
@@ -0,0 +1,6 @@
+<?php 
+
+define("DB_HOST", 'localhost');
+define("DB_NAME", 'ajax_livesearch');
+define("DB_USER", 'root');
+define("DB_PASSWORD", '');
---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>AJAX - Live Search</title>
+</head>
+<body>
+
+
+	<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+	<script type="text/javascript" src="js/live-search.js"></script>
+</body>
+</html>
