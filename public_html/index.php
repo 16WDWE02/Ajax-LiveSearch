e8031388fbd994f4b4a099314d3e314a1bf7b4d2
Richard Hpa <richard.hpa@hotmail.com>
2016-09-02 09:04:36 +1200

    added databse

--- public_html/index.php
+++ public_html/index.php
@@ -3,7 +3,7 @@
 	<title>AJAX - Live Search</title>
 </head>
 <body>
-
+	<h1>Ajax - Live Search</h1>
 
 	<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 	<script type="text/javascript" src="js/live-search.js"></script>
